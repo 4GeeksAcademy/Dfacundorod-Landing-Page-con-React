8ca5c704d5898a3bcb8c3f9ceab1bdfd1d4fe727
Dfacundorod <112908434+Dfacundorod@users.noreply.github.com>
2024-08-23 16:44:42 +0000

    estableciendo ultimos styles

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropType from 'prop-types';
 
 export const Footer = ()=>{
-    return (<div class="fixed-bottom bg-secondary d-flex justify-content-center text-center text-white">
-        <h5>Copyright@YourWebSite 2019</h5>
+    return (<div className="footer fixed-bottom bg-secondary d-flex justify-content-center text-center text-white">
+        <h5 className="my-auto">Copyright@YourWebSite 2019</h5>
     </div>)
 }
