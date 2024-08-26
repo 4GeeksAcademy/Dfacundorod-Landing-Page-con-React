b7dd7be845e9724304b743ef8ab887cd49d2986d
Dfacundorod <112908434+Dfacundorod@users.noreply.github.com>
2024-08-26 23:56:42 +0000

    correccion de ubicacion de componentes

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import { Home } from "./component/home.jsx";
+
+import { NavBar } from "./component/navBar";
+import { Jumbotron } from "./component/jumbotron";
+import { Cards } from "./component/cards";
+import { Footer } from "./component/footer";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+ReactDOM.createRoot(document.getElementById('app')).render(<div className="container-fluid">
+    <NavBar title='Start Boostrap' titleUrl='#'/>
+    <Jumbotron title='A Warm Welcome' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam incidunt nisi nesciunt. Alias voluptatem dolor sit reprehenderit molestiae fugit inventore libero? Dolorum maiores pariatur molestias rem incidunt, tempora provident ex.' label='Call to action!' />
+    <Cards/>
+    <Footer/>
+</div>);
 
